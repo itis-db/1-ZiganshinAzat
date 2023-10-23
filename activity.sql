42e89262ec2136f5f96ef1691a451a065dc81ef0
ZiganshinAzat <azv7_78@mail.ru>
2023-10-23 23:12:57 +0300

    кр 1 уровня Зиганшин Азат 11-207

--- activity.sql
+++ activity.sql
@@ -1,15 +1,9 @@
--- рекурсия 
-WITH RECURSIVE r AS (
-    SELECT 
-        1 AS i, 
-        1 AS factorial
-    
-    UNION 
-
-    SELECT 
-        i+1 AS i, 
-        factorial * (i+1) as factorial 
-    FROM r
-    WHERE i < 10
+with recursive activity_ierarchy(activitytypeid, name, code, parentid, path) as (
+	select table1.activitytypeid, table1.name, table1.code, table1.parentid, table1.code as path 
+	from activity table1 where table1.name = 'КТ 7' -- написать имя нужного элемента, к примеру КТ 7
+	union
+	select table2.activitytypeid, table2.name, table2.code, table2.parentid, activity_ierarchy.path || '->' || table2.code 
+	from activity table2 join activity_ierarchy on (activity_ierarchy.parentid = table2.activityid)
 )
-SELECT * FROM r;
+select "path" from activity_ierarchy where activitytypeid = 1;
+
--- data.sql
+++ data.sql
@@ -1,17 +1,46 @@
-    insert into activitytype(
-    	  activitytypeid
-        ,name
-        ,sysname
-    )
-    select d.*
-    from (
-    	values(1,'Программа','Program')
-        ,(2,'Подпрограмма','SubProgram')
-        ,(3,'Проект','Project')
-        ,(4,'Контракт','Contract')
-        ,(5,'КТ','Point')
-    ) d(activitytypeid, name, sysname)
-    where not exists(
-    	select 1 from activitytype t
-        where t.activitytypeid = d.activitytypeid
-    );
+create table if not exists area(
+ areaid int not null primary key,
+ "name" text not null
+);
+
+create table if not exists activitytype(
+ activitytypeid int not null primary key,
+ "name" text not null,
+ sysname text not null
+);
+
+create table if not exists activity(
+ activityid int not null primary key,
+ activitytypeid int not null references activitytype(activitytypeid),
+ code text not null, 
+ "name" text not null,
+ parentid int references activity(activityid)
+);
+
+create table if not exists contract(
+ contractid int not null references activity(activityid) primary key,
+ areaid int not null references area(areaid)
+);
+
+create table if not exists subprogram(
+ subprogramid int not null references activity(activityid) primary key,
+ indexnum int not null 
+);
+
+create table if not exists project(
+ projectid int not null references activity(activityid) primary key,
+ targerdescr text not null
+);
+
+create table if not exists program(
+ programid int not null references activity(activityid) primary key,
+ indexnum int not null,
+ yearstart int not null,
+ yearfinish int not null
+);
+
+create table if not exists point(
+ pointid int not null references activity(activityid) primary key,
+ plandate date not null,
+ factdate date not null
+);
